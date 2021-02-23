c87b20241f0754deae479563609668dd5a133dcd
apurvapatil03 <apoorvapatil03@gmail.com>
2021-02-24 01:59:13 +0530

    DbConnect.php

--- attendance/login.php
+++ attendance/login.php
@@ -1,36 +1,40 @@
 <?php
 
-    //getting db connection
-    require_once 'DbConnect.php';
+    //use this when cloud instance is not running
+    $footerFile = 'C:\wamp64\www\attendance\login.php';
+    include($footerFile);
 
-    $moodleId=$_POST['moodleId'];
-    $password=md5($_POST['password']);
+    // //getting db connection
+    // require_once 'DbConnect.php';
 
-    //an array to display response
-    $output=array();
+    // $moodleId=$_POST['moodleId'];
+    // $password=md5($_POST['password']);
 
-    $stmt=$conn->prepare("SELECT MoodleID,password FROM registration WHERE MoodleID=? AND password=?");
-    $stmt->bind_param("is",$moodleId,$password);
-    $stmt->execute();
-    $stmt->store_result();
+    // //an array to display response
+    // $output=array();
+
+    // $stmt=$conn->prepare("SELECT MoodleID,password FROM registration WHERE MoodleID=? AND password=?");
+    // $stmt->bind_param("is",$moodleId,$password);
+    // $stmt->execute();
+    // $stmt->store_result();
     
-    if($stmt->num_rows==0){
-        $output['isSuccess']=0;
-        $output['message']="Incorrect MoodleId or Password";
-    }
-    else{
-        $stmt->bind_result($moodleId,$password);
-        $stmt->fetch();
+    // if($stmt->num_rows==0){
+    //     $output['isSuccess']=0;
+    //     $output['message']="Incorrect MoodleId or Password";
+    // }
+    // else{
+    //     $stmt->bind_result($moodleId,$password);
+    //     $stmt->fetch();
         
 
-        $user_details=array(
-            'moodleId'=>$moodleId
-        );
+    //     $user_details=array(
+    //         'moodleId'=>$moodleId
+    //     );
 
-        $output['isSuccess']=1;
-        $output['message']="Login Successfull!";
-        $output['user_details']=$user_details;        
-    }
+    //     $output['isSuccess']=1;
+    //     $output['message']="Login Successfull!";
+    //     $output['user_details']=$user_details;        
+    // }
 
-    echo json_encode($output);
+    // echo json_encode($output);
 ?>

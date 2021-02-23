fd6014b1b89120a0f08eb9d011ac63945e825277
apurvapatil03 <apoorvapatil03@gmail.com>
2021-02-24 01:52:57 +0530

    DbConnect.php

--- attendance/DbConnect.php
+++ attendance/DbConnect.php
@@ -3,25 +3,25 @@
 $footerFile = 'C:\wamp64\www\attendance\DbConnect.php';
 include($footerFile);
 
-// $servername = "localhost";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "mydb";
+// $servername = "34.93.155.184";
 // $username = "root";
-// $password = "";
-// $database = "mydb";
-// // $servername = "34.93.155.184";
-// // $username = "root";
-// // $password = "apkove";
-// // $database = "test"; 
+// $password = "apkove";
+// $database = "test"; 
  
-// //creating a new connection object using mysqli 
-// $conn = new mysqli($servername, $username, $password, $database);
+//creating a new connection object using mysqli 
+$conn = new mysqli($servername, $username, $password, $database);
  
-// //if there is some error connecting to the database
-// //with die we will stop the further execution by displaying a message causing the error 
-// if ($conn->connect_error) {
-//     die("Connection failed: " . $conn->connect_error);
-// }
-// else{
+//if there is some error connecting to the database
+//with die we will stop the further execution by displaying a message causing the error 
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+else{
     
-// }
+}
 
 ?>

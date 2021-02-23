6628e1aa7ea468cc8e775af606287f3d693daddb
apurvapatil03 <apoorvapatil03@gmail.com>
2021-02-23 21:47:56 +0530

    added composer.json

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 function navSlide() {
    
     document.getElementById("addman").onclick = function () {
-        location.href = "www.youtube.com";
+        location.href = "www.youtube.com"; }
     
 }
 

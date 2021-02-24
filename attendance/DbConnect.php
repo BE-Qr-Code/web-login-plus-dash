8efacd769664c316f77d2a1635eedf3a61ecf9f9
apurvapatil03 <apoorvapatil03@gmail.com>
2021-02-24 12:06:13 +0530

    DbConnect.php

--- attendance/DbConnect.php
+++ attendance/DbConnect.php
@@ -1,28 +1,28 @@
 <?php
 
 //use this when cloud instance is not running
-$footerFile = 'C:\wamp64\www\attendance\DbConnect.php';
-include($footerFile);
+// $footerFile = 'C:\wamp64\www\attendance\DbConnect.php';
+// include($footerFile);
 
 // // $servername = "localhost";
 // // $username = "root";
 // // $password = "";
 // // $database = "mydb";
 // $servername = "34.93.155.184";
-// $username = "root";
-// $password = "apkove";
-// $database = "test"; 
+$username = "root";
+$password = "apkove";
+$database = "test"; 
  
-// //creating a new connection object using mysqli 
-// $conn = new mysqli($servername, $username, $password, $database);
+//creating a new connection object using mysqli 
+$conn = new mysqli($servername, $username, $password, $database);
  
-// //if there is some error connecting to the database
-// //with die we will stop the further execution by displaying a message causing the error 
-// if ($conn->connect_error) {
-//     die("Connection failed: " . $conn->connect_error);
-// }
-// else{
+//if there is some error connecting to the database
+//with die we will stop the further execution by displaying a message causing the error 
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+else{
     
-// }
+}
 
 ?>
